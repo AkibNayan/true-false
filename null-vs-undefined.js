a335890b45f484f63e6995ca500306bd597a8f6d
Akib Uddin Nayan <akibnayan182@gmail.com>
2022-05-04 16:30:28 +0600

    All Set

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// let pakhi;
+// function add(num1, num2) {
+//     const result = num1 + num2;
+// }
+// function sub(num1, num2) {
+//     console.log(num2);
+// }
+//const output = add(32, 43);
+//console.log(output);
+//const res = sub(33);
+
+// const premik = { name: "Dude", phone: 234343 };
+// console.log(premik.wife);
+
+// const fun = undefined;
+// console.log(fun);
+
+const ages = [23, 32, 45];
+console.log(ages[33]);
